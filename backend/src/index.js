ea545b9d57add9b1ffeee219ce951d373f462897
pupsaeroyi <pup.natapat@gmail.com>
2026-04-08 17:49:20 +0700

    fix: replace bcrypt with bcryptjs for faster builds

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { pool } from "./db.js";
 import jwt from "jsonwebtoken";
